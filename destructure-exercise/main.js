3654de265274272485e300a0a8c661dbfdcadf76
Emad <101589440+e-Ash1@users.noreply.github.com>
2023-10-09 21:42:31 -0400

    Completed set of exercises for destructuring in JS

---
+++ destructure-exercise/main.js
@@ -0,0 +1,75 @@
+//Object Destructuring #1:
+
+// let facts={numPlanets:8, yearNeptuneDiscovered:1846,};
+// let {numPlanets, yearNeptuneDiscovered}=facts;
+// console.log(numPlanets, yearNeptuneDiscovered); Expected output: 8 1846
+
+//Object Destructuring #2:
+
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); //Expected Output: {yearsNeptuneDiscovered: 1846, yearsMarsDiscovered: 1659}
+
+//Object Destructuring #3:
+
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // Expected output: Your name is Alejandro and you like purple
+  getUserData({firstName: "Melissa"}) // Your name is Melissa and you like green 
+  getUserData({}); // Your name is undefined and you like green 
+
+//Array Destructuring #1:
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marissa
+console.log(third); // Chi
+
+//Array Destructuring #2:
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // Raindrops on roses
+  console.log(whiskers); // whiskers on kittens
+  console.log(aFewOfMyFavoriteThings); // [Bright copper kettles, warm woolen mittens, Brown paper packages tied up with strings] 
+
+//Array Destructuring #3:
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10,30,20]
+
+
+/*
+## **raceResults()**
+
+Write a function called ***raceResults*** which accepts a single array argument. It should return an object with the keys ***first***, ***second***, ***third***, and ***rest***.
+
+- first: the first element in the array
+- second: the second element in the array
+- third: the third element in the array
+- rest: all other elements in the array
+
+**Write a *one line* function to make this work using**
+
+- An arrow function
+- Destructuring
+- `Enhanced` object assignment (same key/value shortcut)
+*/ 
+
+let raceResults=([first,second,third, ...rest])=>{return first,second,third,rest}
+const results = raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+console.log(results);
