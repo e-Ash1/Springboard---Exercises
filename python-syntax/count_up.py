fc497e95042e527dffedd3e954a4f48cf9e21e90
Emad <101589440+e-Ash1@users.noreply.github.com>
2023-11-01 21:03:46 -0400

    Completed set of exercises, practicing Python syntax

---
+++ python-syntax/count_up.py
@@ -0,0 +1,21 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+    for nums in range(start,(stop+1)):
+        print(nums)
+
+
+count_up(5, 7)    
+count_up(4,6)    
